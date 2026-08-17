e6a2a8a8ebad7bc812c2b8c33346b6d27592db4e
Reis <151974575+gogito28@users.noreply.github.com>
2026-08-17 12:52:49 +0300

    Add EV server online handler

--- netlify/functions/ev.js
+++ netlify/functions/ev.js
@@ -1,1 +1,12 @@
-
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      ok: true,
+      message: "EV server is online."
+    })
+  };
+};
